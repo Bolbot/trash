dc752283242130214414e4646cd12927c4fd92e4
Bogdan Bolbot <Bolbot2018@outlook.com>
2019-10-13 00:16:40 +0300

    3. C version now with daemonization in C++ style
    also some updates to CMakeLists.txt and further warning fix

--- main.cpp
+++ main.cpp
@@ -13,51 +13,13 @@
 #include "server.h"
 #include "utils.h"
 
-extern FILE *_myoutput;
-
 int main(int argc, char **argv)
 {
-	_myoutput = stdout;
+	parse_program_options(argc, argv);
 
-	setsignals();
-	char *desired_ip = NULL;
-	char *desired_port = NULL;
-	memset(directory, 0, PATHSIZE);
+	daemonize();
 
-	const char *arguments = "h:p:d:f:";	int arg;
-	if(argc < 2)
-	{
-		fprintf(_myoutput, "Usage: %s -h <ip> -p <port> -d <directory> -f <logfile>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	else	while((arg = getopt(argc, argv, arguments)) != -1)
-		switch(arg)
-		{
-		case 'h': desired_ip = optarg;		if(VERBOSE) fprintf(_myoutput, "\t[IP %s]\n", desired_ip); break;
-		case 'p': desired_port = optarg;	if(VERBOSE) fprintf(_myoutput, "\t[Port %s]\n", desired_port); break;
-		case 'd':
-			{
-				strncpy(directory, optarg, PATHSIZE - 1);
-				if(directory[strlen(directory) - 1] != '/') strcat(directory, "/");
-				if(VERBOSE) fprintf(_myoutput, "\t[Directory %s]\n", directory);
-				break;
-			}
-		case 'f': if(VERBOSE) fprintf(stdout, "\t[Hence this line output is redirected to %s]\n", optarg); redirect_output(optarg); break;
-		default: fprintf(_myoutput, "There is no such argument by design. Shall ignore something. Be sure to use -h -p -d -f\n");
-	};
-
-	pid_t pid = fork();
-	if(pid == -1) { fprintf(_myoutput, "Fail of fork: %s\n", strerror(errno)); exit(EXIT_FAILURE); }
-	else if(pid) exit(EXIT_SUCCESS);
-
-	umask(0);
-
-	pid_t session_id = setsid();
-	if(session_id == (pid_t)-1) fprintf(_myoutput, "Fail of setsid(): %s\n", strerror(errno));
-
-	if(chdir("/") == -1) fprintf(_myoutput, "Fail of chdir(\"/\"): %s\n", strerror(errno));
-
-	server(desired_ip, desired_port);
+	server(server_ip.data(), server_port.data());
 
 	return 0;
 }
--- utils.h
+++ utils.h
@@ -1,14 +1,102 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include <string>
+#include <iostream>
+#include <exception>
+#include <fstream>
+#include <ctime>
+#include <mutex>
+
+#include <boost/program_options.hpp>
+
 #include "defines.h"
 
-extern char directory[PATHSIZE];
+extern std::mutex cerr_mutex;
+#define LOG_CERROR(x) log_errno((__func__), (__FILE__), (__LINE__), (x))
+void log_errno(const char *function, const char *file, size_t line, const char *message) noexcept;
+
+extern std::string server_ip;
+extern std::string server_port;
+extern std::string server_directory;
+
+void parse_program_options(int argc, char **argv) noexcept;
+
+void daemonize() noexcept;
+
+class log_redirector final
+{
+private:
+	static constexpr char log_file_out_name[] = "the_server_out.log";
+ 	static constexpr char log_file_err_name[] = "the_server_err.log";
+	static constexpr char log_file_log_name[] = "the_server_log.log";
+
+	class redirected_stream final
+	{
+	private:
+		std::ostream *log_stream;
+		std::streambuf *old_buffer;
+		std::ofstream own_ofstream;
+	public:
+		redirected_stream(std::ostream &where_from, const char *dest_file)
+			: log_stream{ &where_from }, old_buffer{ log_stream->rdbuf() }
+		{
+			own_ofstream.open(dest_file);
+			if (!own_ofstream.is_open())
+				throw std::runtime_error("Failed to open file for logging");
+
+			log_stream->rdbuf(own_ofstream.rdbuf());
+		}
+
+		~redirected_stream()
+		{
+			log_stream->rdbuf(old_buffer);
+		}
+	};
+
+	redirected_stream *redirected_cout = nullptr;
+	redirected_stream *redirected_cerr = nullptr;
+	redirected_stream *redirected_clog = nullptr;
+
+protected:
+	log_redirector() :
+		redirected_cout{ new redirected_stream(std::cout, log_file_out_name) },
+		redirected_cerr{ new redirected_stream(std::cerr, log_file_err_name) },
+		redirected_clog{ new redirected_stream(std::clog, log_file_log_name) }
+	{
+	}
+public:
+	static log_redirector &instance()
+	{
+		static log_redirector object;
+		return object;
+	}
+	log_redirector(const log_redirector &) = delete;
+	log_redirector &operator=(const log_redirector &) = delete;
+	~log_redirector()
+	{
+		delete redirected_cout;
+		delete redirected_cerr;
+		delete redirected_clog;
+	}
+};
+
+void signal_handler(int signal_number) noexcept;
+
+void set_signal(int signal_number) noexcept;
+
+void set_signals() noexcept;
+
+time_t current_time_t() noexcept;
+
+std::string time_t_to_string(time_t seconds_since_epoch);
+
+void atexit_terminator() noexcept;
+
+[[noreturn]] void terminate_handler() noexcept;
 
 size_t get_file_size(const char *fpath);
 int prepare_file_to_send(const char *rel_path, size_t *file_size, char *mime_type);
-FILE *redirect_output(const char *file_addr);
-void setsignals();
 int get_file_mime_type(const char *fpath, char *mimetype);
 
 #endif
